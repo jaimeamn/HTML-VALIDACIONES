6c2c58b0c91de3b34d07cfaeab51f124cfcd6273
jaimeamn1 <jaimeamn1@gmail.com>
2022-11-05 11:03:07 -0500

    validaciones de fechas, nombre, formulario de registro

---
+++ js/validaciones.js
@@ -0,0 +1,26 @@
+const inputNacimiento = document.querySelector("#birth")
+
+inputNacimiento.addEventListener("blur", (evento) =>{
+    validarNacimiento(evento.target)
+
+});
+function validarNacimiento(input){
+    const fechaCliente = new Date(input.value);
+    let mensaje = ""
+    if (!mayorDeEdad(fechaCliente)){
+        mensaje = "Debes tener al menos 18 años de edad";
+    }
+    input.setCustomValidity(mensaje);
+}
+function mayorDeEdad(fecha){
+    const fechaActual = new Date();
+    const diferenciaFecha = new Date(
+        fecha.getUTCFullYear() + 18,
+        fecha.getUTCMonth(),
+        fecha.getUTCDate()
+    );
+    console.log(fechaActual)
+    console.log(diferenciaFecha)
+
+    return diferenciaFecha < fechaActual;
+}
